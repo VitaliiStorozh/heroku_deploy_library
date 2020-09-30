53f407697d9f8811c66ce3a3b14f837b9069d235
yurasenchuk <63191681+yurasenchuk@users.noreply.github.com>
2020-09-30 15:15:26 +0200

    Solutions for all order functionality

--- order/urls.py
+++ order/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from order import views
 
 urlpatterns = [
-
+    path("", views.get_orders, name="get_orders"),
+    path("new_order/<int:id>/", views.new_order, name="new_order"),
+    path("delete/<int:id>", views.delete_order, name="delete_order"),
 ]
